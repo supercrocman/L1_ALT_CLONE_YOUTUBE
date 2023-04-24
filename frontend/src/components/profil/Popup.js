55dd9805ec7ea272033a9dc6c1d70e3737b0ab60
Elisa Semedo <elisasemedo07@gmail.com>
2023-04-24 15:40:45 +0200

    popup

---
+++ frontend/src/components/profil/Popup.js
@@ -0,0 +1,30 @@
+import { Modal, Paper } from '@mui/material';
+
+function Popup({ open, handleClose, children }) {
+    return (
+        <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+        >
+            <Paper
+                sx={{
+                    position: 'relative',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    width: 400,
+                    bgcolor: '#ffffff',
+                    border: '2px solid #000',
+                    boxShadow: 24,
+                    p: 4,
+                    border: 'none',
+                }}
+            >
+                {children}
+            </Paper>
+        </Modal>
+    );
+}
+export default Popup;
