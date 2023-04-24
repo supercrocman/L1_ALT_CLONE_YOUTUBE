e8633548e50d28c85a45b8429c96a6ff0cf40b93
Elisa Semedo <elisasemedo07@gmail.com>
2023-04-24 16:25:42 +0200

    Subscribe v1

--- frontend/src/components/profil/Subscribe.js
+++ frontend/src/components/profil/Subscribe.js
@@ -1,3 +1,5 @@
+import { Box, Button, TextField, Typography } from '@mui/material';
+import { useState } from 'react';
 import Popup from './Popup';
 
 function Subscribe() {
@@ -5,6 +7,36 @@ function Subscribe() {
     const handleClose = () => {
         setOpen(false);
     };
-    return <Popup open={open}></Popup>;
+    return (
+        <Box>
+            <Button
+                onClick={() => {
+                    setOpen(true);
+                }}
+            >
+                Ouvrir Pop up
+            </Button>
+            <Popup open={open} handleClose={handleClose}>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Typography variant="h2">Inscription</Typography>
+                    <TextField label="Pseudo" variant="filled" />
+                    <TextField label="Email" variant="filled" />
+                    <TextField label="Mot de passe" variant="filled" />
+                    <TextField
+                        label="Confirmation mot de passe"
+                        variant="filled"
+                    />
+                    <Button onClick={handleClose}>S'inscrire</Button>
+                </Box>
+            </Popup>
+        </Box>
+    );
 }
 export default Subscribe;
