82f74180f177071fa9c8ddfa981d70e991139ace
Mehdi <mbourouih94@gmail.com>
2023-04-26 17:02:37 +0200

    API en travaux

--- backend/index.js
+++ backend/index.js
@@ -68,6 +68,8 @@ app.use('/api', require('./routes/search'));
 
 app.use('/api', require('./routes/channelinfos'));
 
+
+
 app.listen(port, () => {
     logger.info(`Example app listening on port ${port}`);
 });
