f0f3e733d5ad7d216efdd9ff3559a4780af2fd60
DevMehd <60779348+DevMehd@users.noreply.github.com>
2023-04-27 21:52:50 +0200

    Update channelInfos.js

--- backend/routes/channelInfos.js
+++ backend/routes/channelInfos.js
@@ -4,24 +4,6 @@ const router = express.Router();
 const db = require('../services/sequelize');// Importez le modèle User
 const { Op } = require('sequelize');
 
-router.get('/users', async (req, res) => {
-  // if (req.body.q === undefined) return res.status(400).send('No query provided');
-  // if (req.body.q.length < 3) return res.status(400).send('Query too short');
-const identifier = req.headers.identifier;
-  try {
-    const users = await db.User.findAll({
-      attributes: ['id', 'identifier', 'name', 'verified', 'description'],
-      where: {
-        identifier: identifier
-      }
-    });
-    res.json(users);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Erreur lors de la récupération des utilisateurs');
-  }
-});
-
 router.get('/user/:identifier', async (req, res) => {
   try {
     const userIdentifier = req.params.identifier;
