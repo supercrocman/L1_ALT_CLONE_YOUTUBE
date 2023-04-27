220676f25c1dafa9802ccc5c371f2146332f7726
DevMehd <60779348+DevMehd@users.noreply.github.com>
2023-04-27 21:45:20 +0200

    Update channelInfos.js

--- backend/routes/channelInfos.js
+++ backend/routes/channelInfos.js
@@ -1,18 +1,18 @@
 // routes/channelinfos.js
 const express = require('express');
+
 const router = express.Router();
 const db = require('../services/sequelize'); // Importez le modèle User
-const { Op } = require('sequelize');
 
 router.get('/users', async (req, res) => {
     // if (req.body.q === undefined) return res.status(400).send('No query provided');
     // if (req.body.q.length < 3) return res.status(400).send('Query too short');
-    const identifier = req.headers.identifier;
+    const { identifier } = req.headers;
     try {
         const users = await db.User.findAll({
             attributes: ['id', 'identifier', 'name', 'verified', 'description'],
             where: {
-                identifier: identifier,
+                identifier,
             },
         });
         res.json(users);
@@ -64,14 +64,13 @@ router.get('/user/:identifier', async (req, res) => {
                 user: {
                     informations: user,
                     subCount,
-                    videoCount: videoCount,
+                    videoCount,
                     videos,
                 },
             });
         } else {
             res.status(404).send('Utilisateur non trouvé');
         }
-        ('');
     } catch (error) {
         // res.status(500).send(error);
         res.status(500).send("Erreur lors de la récupération de l'utilisateur");
