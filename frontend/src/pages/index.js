0d3a883f918c1604aa605fcdd0a4d9d6229c835b
Kevin VONG <vongkevin.c92@gmail.com>
2023-04-27 15:34:21 +0200

    add:Front Search Bar v1

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,8 +1,10 @@
 import AccountMenu from './includes/accountmenu'
+import SearchBar from '@/components/SearchBar'
 
 export default function Home() {
   return (
     <div>
+      <SearchBar />
       <AccountMenu></AccountMenu>
     </div>
   )
