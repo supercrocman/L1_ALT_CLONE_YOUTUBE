a14e1f551f09403b324465b26b0407ee5f8af45c
ISMAILOV <elmani3107@gmail.com>
2023-04-28 16:43:15 +0200

    push back
    back start project

---
+++ backend/index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const db = require('./services/sequelize');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors(
+    {
+        origin: '*',
+    }
+))
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        //
+        // - Write all logs with importance level of `error` or less to `error.log`
+        // - Write all logs with importance level of `info` or less to `combined.log`
+        //
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'combined.log' }),
+    ],
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(
+        new winston.transports.Console({
+            format: winston.format.simple(),
+        })
+    );
+}
+
+db.sequelize
+    .sync()
+    .then(() => {
+        logger.info('Synced db.');
+    })
+    .catch((err) => {
+        logger.info(`Failed to sync db: ${err.message}`);
+    });
+
+const port = 3001;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.use('/api', require('./routes/search'));
+
+app.use('/api', require('./routes/channelinfos'));
+
+
+
+app.listen(port, () => {
+    logger.info(`Example app listening on port ${port}`);
+});
