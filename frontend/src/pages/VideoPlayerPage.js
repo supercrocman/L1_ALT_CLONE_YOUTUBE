9ba81400ef46f91b161a42073f0c49b9ead12ece
Léandre <44951955+leandre0@users.noreply.github.com>
2023-05-02 18:08:17 +0200

    Update VideoPlayerPage.js

--- frontend/src/pages/VideoPlayerPage.js
+++ frontend/src/pages/VideoPlayerPage.js
@@ -5,14 +5,14 @@ import VideoInformation from "../component/VideoInformation";
 
 export default function VideoPlayerPage() {
     const styles = {
-        background: "linear-gradient(119deg, rgba(36,0,23,1) 0%, rgba(105,9,121,1) 26%, rgba(209,0,255,1) 100%);",
+        background: "#121212",
         height: "100vh",
         width: "100vw",
         position: "absolute",
         top: 0,
         left: 0,
         display: "flex",
-    };
+        }
     return (
             <div style={styles}>
                 <Player></Player >
