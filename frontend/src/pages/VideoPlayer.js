03fa8cf68fabab118c0f4efdf13b4251cc6be39c
Léandre <44951955+leandre0@users.noreply.github.com>
2023-05-02 19:05:46 +0200

    update compo

--- frontend/src/pages/VideoPlayer.js
+++ frontend/src/pages/VideoPlayer.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { Player } from "../component/Player";
-import VideoComments from "../component/VideoComments";
-import VideoInformation from "../component/VideoInformation";
+import { Player } from "../components/Player";
+import VideoComments from "../components/VideoComments";
+import VideoInformation from "../components/VideoInformation";
 import { Container } from "@mui/material";
 
 export default function VideoPlayerPage() {
     return (
             <div>
                 <Container/>
-                <Player/>
+                <Player></Player>
                 <VideoComments />
                 {/*<VideoInformation />*/}
             </div>
