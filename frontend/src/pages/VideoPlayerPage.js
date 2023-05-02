daad6a392931760e31ebe8b430c1d5b3dc49c476
Léandre <44951955+leandre0@users.noreply.github.com>
2023-05-02 18:19:26 +0200

    test import style

--- frontend/src/pages/VideoPlayerPage.js
+++ frontend/src/pages/VideoPlayerPage.js
@@ -2,19 +2,20 @@ import React from "react";
 import { Player } from "../component/Player";
 import VideoComments from "../component/VideoComments";
 import VideoInformation from "../component/VideoInformation";
+import { styled } from "@mui/material/styles";
 
 export default function VideoPlayerPage() {
-    const styles = {
-        background: "#121212",
-        height: "100vh",
-        width: "100vw",
-        position: "absolute",
-        top: 0,
-        left: 0,
-        display: "flex",
-        }
+    // const styles = {
+    //     background: "#121212",
+    //     height: "100vh",
+    //     width: "100vw",
+    //     position: "absolute",
+    //     top: 0,
+    //     left: 0,
+    //     display: "flex",
+    //     }
     return (
-            <div style={styles}>
+            <div style={styled}>
                 <Player></Player >
                 <VideoComments />
                 {/*<VideoInformation />*/}
