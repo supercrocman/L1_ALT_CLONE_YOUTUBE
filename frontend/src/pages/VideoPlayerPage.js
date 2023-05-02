d716a03aabe6c803896329a95045e8395366ecc3
Léandre <44951955+leandre0@users.noreply.github.com>
2023-05-02 17:50:26 +0200

    relative position

--- frontend/src/pages/VideoPlayerPage.js
+++ frontend/src/pages/VideoPlayerPage.js
@@ -6,8 +6,8 @@ import VideoInformation from "../component/VideoInformation";
 export default function VideoPlayerPage() {
     const styles = {
         background: "linear-gradient(119deg, rgba(36,0,23,1) 0%, rgba(105,9,121,1) 26%, rgba(209,0,255,1) 100%);",
-        height: "100vh",
-        width: "100vw",
+        height: "100%",
+        width: "100%",
         position: "absolute",
         top: 0,
         left: 0,
