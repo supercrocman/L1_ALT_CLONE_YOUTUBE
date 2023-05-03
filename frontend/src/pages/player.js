76c5dc03fe5deebccce856eca53aaf89d41f4b80
Léandre <44951955+leandre0@users.noreply.github.com>
2023-05-03 10:56:49 +0200

    just changed the name

--- frontend/src/pages/player.js
+++ frontend/src/pages/player.js
@@ -10,7 +10,7 @@ export default function VideoPlayerPage() {
                 <Container/>
                 <Player/>
                 <VideoComments/>
-                {/*<VideoInformation />*/}
+                {/* <VideoInformation/> */}
             </div>
     );
 }
